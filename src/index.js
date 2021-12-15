5e7e445d30ca1a13d04dd2b83d44f85669bba253
2jun0 <soo28819@naver.com>
2021-12-16 02:29:42 +0900

    modify: replace the execa module with execFile.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 'use strict'
 
 const dargs = require('dargs')
-const execa = require('execa')
+const util = require('util')
+const execFile = util.promisify(require('child_process').execFile)
 const { YOUTUBE_DL_PATH } = require('./options.js')
 
 const args = (url, flags = {}) =>
@@ -15,8 +16,8 @@ const isJSON = (str = '') => str.startsWith('{')
 const parse = ({ stdout }) => (isJSON(stdout) ? JSON.parse(stdout) : stdout)
 
 const create = youtubedlPath => {
-  const fn = (url, flags, opts) => fn.exec(url, flags, opts).then(parse)
-  fn.exec = (url, flags, opts) => execa(youtubedlPath, args(url, flags), opts)
+  const fn = (url, flags) => fn.exec(url, flags).then(parse)
+  fn.exec = (url, flags) => execFile(youtubedlPath, args(url, flags))
   return fn
 }
 
